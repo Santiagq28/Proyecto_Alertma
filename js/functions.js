937dfcaa8815425d09a45ff259b4a583f5c25cb5
Santiago Guevara <saguevaraq@ufpso.edu.co>
2024-05-07 00:21:33 -0500

    Update 0.0.2 Register

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,12 @@
 var name;
+var name2;
 var lastname;
+var lastname2;
+var date;
+var number;
+var genero;
+var age;
+
 var registro;
 var page;
 console.log("Hola");
@@ -7,21 +14,38 @@ console.log("Hola");
 function cargar(){
     document.getElementById("name").focus();
     page = document.getElementById("page");
-    page.style.visibility = "hidden"
+    page.style.display = "none";
 }
 function datos(){
     name = document.getElementById("name").value;
+    name2 = document.getElementById("name2").value;
     lastname = document.getElementById("lastname").value;
+    lastname2 = document.getElementById("lastname2").value;
+    date = document.getElementById("date").value;
+    number = document.getElementById("number").value;
+    genero = document.getElementById("gender").value;
+    age = document.getElementById("age").value;
+
     registro = document.getElementById("registro");
-    if(name != "" && lastname != ""){
+
+    if(name != "" && lastname != "" && lastname2 != "" && date != "" && number != "" && genero != "" && age != ""){
         registro.style.visibility = "hidden";
-        document.getElementById("info").innerText = name+" "+lastname;
-        document.getElementById("info2").innerText = name+" "+lastname;
-        page.style.visibility = "visible";
+
+        document.getElementById("info").innerText = name;
+        document.getElementById("info2").innerText = "Bienvenid@ " + name
+        document.getElementById("names").innerText += name+ " "+name2;
+        document.getElementById("lastnames").innerText += lastname+" "+lastname2;
+        document.getElementById("dates").innerText += date;
+        document.getElementById("cel").innerText += number;
+        document.getElementById("genero").innerText += genero;
+        document.getElementById("edad").innerHTML += age;
+
+
+        page.style.display = "flex";
         
 
     }else{
-        alert("Ingrese sus datos")
+        Swal.fire("Completa todos los campos");
     }
     
     
