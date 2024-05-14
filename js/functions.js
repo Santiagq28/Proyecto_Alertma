8d4eee1e4ff393ccb01d7288bb44307ce5cdc309
Santiago Guevara <saguevaraq@ufpso.edu.co>
2024-05-14 16:08:30 -0500

    Add Prediagnostic 0.0.4

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,7 @@ var date;
 var number;
 var genero;
 var age;
+var lastname;
 
 var registro;
 var page;
@@ -15,10 +16,12 @@ function cargar(){
 }
 function datos(){
     name = document.getElementById("name").value;
+    lastname = document.getElementById("lastname").value;
     date = document.getElementById("date").value;
     number = document.getElementById("number").value;
     genero = document.getElementById("gender").value;
     age = document.getElementById("age").value;
+
 
     registro = document.getElementById("registro");
 
@@ -26,9 +29,9 @@ function datos(){
         registro.style.visibility = "hidden";
 
         document.getElementById("info").innerText = name;
-        document.getElementById("info2").innerText = "Bienvenid@ " + name
+        document.getElementById("info2").innerText = "Bienvenid@ " + name;
         document.getElementById("names").innerText += name
-        document.getElementById("lastnames").innerText += lastname
+        document.getElementById("lastnames").innerText += lastname;
         document.getElementById("dates").innerText += date;
         document.getElementById("cel").innerText += number;
         document.getElementById("genero").innerText += genero;
